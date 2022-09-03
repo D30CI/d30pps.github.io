dc70c489c7c8f46054516f9bc4e192212b50b2f0
D30CI <insinyur.koplax@gmail.com>
2022-09-03 16:10:00 +0700

    Revisi Tampilan 03/09/2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,14 @@ for(var i = 0; i < sidebarItems.length; i++) {
 }
 
 
+window.addEventListener('resize', (event) => {
+    var w = window.innerWidth;
+    if(w < 1200) {
+        document.getElementById('sidebar').classList.remove('active');
+    }else{
+        document.getElementById('sidebar').classList.add('active');
+    }
+});
 
 document.querySelector('.burger-btn').addEventListener('click', () => {
     document.getElementById('sidebar').classList.toggle('active');
