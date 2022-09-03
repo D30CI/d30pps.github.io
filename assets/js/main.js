a6c1ae21dc64ca1c063a67ff21b5c9957976561b
D30CI <insinyur.koplax@gmail.com>
2022-09-03 15:53:19 +0700

    Revisi Tampilan 03/09/2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,7 @@ for(var i = 0; i < sidebarItems.length; i++) {
     })
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    var w = window.innerWidth;
-    if(w < 1200) {
-        document.getElementById('sidebar').classList.remove('active');
-    }
-});
-window.addEventListener('resize', (event) => {
-    var w = window.innerWidth;
-    if(w < 1200) {
-        document.getElementById('sidebar').classList.remove('active');
-    }else{
-        document.getElementById('sidebar').classList.add('active');
-    }
-});
+
 
 document.querySelector('.burger-btn').addEventListener('click', () => {
     document.getElementById('sidebar').classList.toggle('active');
@@ -47,5 +34,3 @@ if(typeof PerfectScrollbar == 'function') {
     });
 }
 
-// Scroll into active sidebar
-document.querySelector('.sidebar-item.active').scrollIntoView(false)
