72250fb31e7970dd0b7816e3c051e85fe42fd1f1
D30CI <insinyur.koplax@gmail.com>
2022-09-20 08:46:01 +0700

    edit css 20/09/2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,10 +40,5 @@ document.querySelector('.sidebar-hide').addEventListener('click', () => {
 
 
 // Perfect Scrollbar Init
-if(typeof PerfectScrollbar == 'function') {
-    const container = document.querySelector(".sidebar-wrapper");
-    const ps = new PerfectScrollbar(container, {
-        wheelPropagation: false
-    });
-}
+
 
